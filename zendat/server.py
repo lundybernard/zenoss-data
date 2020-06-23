f811478fe872a43f457be91e6abd47cf01d43818
LundyBernard <lundy.bernard@gmail.com>
2020-06-22 19:10:26 -0500

    add metrics serveices to flask server

--- zendat/server.py
+++ zendat/server.py
@@ -1,13 +1,26 @@
 from flask import Flask
 
 from zendat.lib import hello_world
+from zendat.metricdict import MetricDictionaryClient
 
 app = Flask(__name__)
+mdc = MetricDictionaryClient()
+
 
 
 @app.route('/')
 def hello_api():
     return hello_world()
+
+
+@app.route('/get_metrics')
+def get_metrics_api():
+    return(mdc.get_metrics())
+
+
+@app.route('/get_metric/<name>')
+def get_metric_api(name):
+    return(mdc.get_metric(name))
 
 
 def start_server(host='0.0.0.0', port='5000', debug=True):
--- zendat/tests/common_api_tests.py
+++ zendat/tests/common_api_tests.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 class CommonAPITest(object):
@@ -8,6 +9,27 @@ class CommonAPITest(object):
     and as local service.
     '''
 
-    def test_compose_webservice_exists(self):
-        out = requests.get(self.service_address)
-        self.assertEqual(out.text, 'Hello World!')
+    def test_compose_webservice_exists(t):
+        ret = requests.get(t.service_address)
+        ret.raise_for_status()
+        t.assertEqual(ret.text, 'Hello World!')
+
+    def test_get_metrics(t):
+        ret = requests.get(f'{t.service_address}/get_metrics')
+        ret.raise_for_status()
+        data = json.loads(ret.text)
+
+        # Metrics contains these Keys
+        t.assertIn('metrics', data)
+        t.assertIn('totalCount', data)
+
+    def test_get_metric_by_name(t):
+        ret = requests.get(f'{t.service_address}/get_metrics')
+        ret.raise_for_status()
+        data = json.loads(ret.text)
+        ex = data['metrics'].pop()
+
+        ret = requests.get(f'{t.service_address}/get_metric/{ex["name"]}')
+        data = json.loads(ret.text)
+
+        t.assertEqual(data, ex)
